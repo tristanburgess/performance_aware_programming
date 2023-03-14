d21ca0b8329b552f26ffb17c35630ea119eafc86
Tristan Burgess <tburgessdev@gmail.com>
2023-03-14 11:27:35 -0400

    sim8086: update tests

--- hw/sim8086/tests/data/jmp.asm
+++ hw/sim8086/tests/data/jmp.asm
@@ -1,15 +1,27 @@
 ; ========================================================================
+;
+; (C) Copyright 2023 by Molly Rocket, Inc., All Rights Reserved.
+;
+; This software is provided 'as-is', without any express or implied
+; warranty. In no event will the authors be held liable for any damages
+; arising from the use of this software.
+;
+; Please see https://computerenhance.com for further information
+;
+; ========================================================================
+
+; ========================================================================
 ; LISTING 41
 ; ========================================================================
 
 bits 16
 
 test_label0:
-jne test_label1
-jne test_label0
+jnz test_label1
+jnz test_label0
 test_label1:
-jne test_label0
-jne test_label1
+jnz test_label0
+jnz test_label1
 
 label:
 je label
